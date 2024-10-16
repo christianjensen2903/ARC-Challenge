a36200de4018e2a840327c6a787e50b78d3e5120
christianjensen2903 <cmj290300@gmail.com>
2024-10-16 09:42:19 +0200

    ✨ Diverse solutions

--- solver.py
+++ solver.py
@@ -9,6 +9,8 @@ from langchain_core.language_models.chat_models import ChatGeneration
 from run_program import run_program
 import numpy as np
 from langsmith import traceable
+from sklearn.metrics import pairwise_distances  # type: ignore
+from sklearn.cluster import KMeans  # type: ignore
 
 
 class Solver(ABC):
@@ -24,6 +26,7 @@ class Solver(ABC):
         self.base_prompt_builder = BasePromptBuilder(formatter)
         self.num_examples = num_examples
         self.examples = examples[: self.num_examples]
+        self.k = 2
 
     @abstractmethod
     def solve(self, demonstrations: list[Demonstration]) -> tuple[str, float]:
@@ -67,6 +70,7 @@ class COTSolver(Solver):
     ):
         super().__init__(model, formatter, num_examples)
         self.num_solutions = num_solutions
+        self.accuracy_cutoff_pct = 50
         self.cost = 0.0
 
     def _predict(
@@ -142,7 +146,7 @@ Please solve the following puzzle.
         return solutions
 
     @traceable(run_type="retriever", name="get_predictions")
-    def _get_predictions(
+    def _trace_predictions(
         self, demonstrations: list[Demonstration], solutions: list[str]
     ) -> list[dict]:
         """
@@ -182,33 +186,85 @@ Stderr:
             for i in range(len(predictions))
         ]
 
-    @traceable(run_type="retriever", name="rank_solutions")
-    def _rank_solutions(
-        self, demonstrations: list[Demonstration], solutions: list[str]
-    ) -> list[dict]:
+    def _get_diversity_matrix(self, predictions: list[list[np.ndarray]]) -> np.ndarray:
+        diversity_matrix = []
+        for preds in predictions:
+            row = []
+            for other_preds in predictions:
+                inner_row = []
+                for i, pred in enumerate(preds):
+                    inner_row.append(self._hamming_distance(pred, other_preds[i]))
 
-        scores = []
+                row.append(np.mean(inner_row))
+            diversity_matrix.append(row)
+
+        return np.array(diversity_matrix)
+
+    def _get_predictions(
+        self, demonstrations: list[Demonstration], solutions: list[str]
+    ) -> list[list[np.ndarray]]:
         predictions = []
         for solution in solutions:
             preds = self._predict(demonstrations, solution)
             predictions.append(preds)
-            distances = []
+
+        return predictions
+
+    def _score_solutions(
+        self, demonstrations: list[Demonstration], predictions: list[list[np.ndarray]]
+    ) -> list[float]:
+        scores: list = []
+        for preds in predictions:
+            distances: list[float] = []
             for i, demonstration in enumerate(demonstrations):
                 pred = preds[i]
                 truth = demonstration.output
                 distance = self._hamming_distance(truth, pred)
                 distances.append(distance)
-
-            score = self._geometric_mean(np.array(distances))
+            score = np.mean(distances)
             scores.append(score)
 
-        ranks = np.argsort(scores)
+        return scores
+
+    def _get_top_indices(self, scores: list[float]) -> list[int]:
+
+        cutoff = int(len(scores) * self.accuracy_cutoff_pct / 100)
+        top_models_indices = np.argsort(scores)[:cutoff]
+
+        return top_models_indices.tolist()
+
+    # TODO: fix Header error
+    @traceable(run_type="retriever", name="rank_solutions")
+    def _rank_solutions(
+        self, demonstrations: list[Demonstration], solutions: list[str]
+    ) -> list[dict]:
+        predictions = self._get_predictions(demonstrations, solutions)
+        scores = self._score_solutions(demonstrations, predictions)
+
+        top_indices = self._get_top_indices(scores)
+        top_predictions = [predictions[i] for i in top_indices]
+        top_solutions = [solutions[i] for i in top_indices]
+        top_scores = [scores[i] for i in top_indices]
+
+        diversity_matrix = self._get_diversity_matrix(top_predictions)
+        kmeans = KMeans(n_clusters=self.k, random_state=0).fit(diversity_matrix)
+
+        diverse_solutions: list[str] = []
+        diverse_scores: list[float] = []
+        for i in range(self.k):
+            cluster_indices = np.where(kmeans.labels_ == i)[0].astype(int)
+            cluster_solutions = [top_solutions[i] for i in cluster_indices]
+            cluster_scores = [top_scores[i] for i in cluster_indices]
+            diverse_solutions.append(cluster_solutions[np.argmin(cluster_scores)])
+            diverse_scores.append(np.min(cluster_scores))
+
+        ranks = np.argsort(diverse_scores)
         return [
             {
-                "page_content": solutions[i],
+                "page_content": diverse_solutions[i],
                 "type": "Document",
                 "metadata": {
-                    "score": scores[i],
+                    "score": diverse_scores[i],
                     "index": i,
                 },
             }
@@ -218,6 +274,6 @@ Stderr:
     def solve(self, demonstrations: list[Demonstration]) -> tuple[str, float]:
         self.cost = 0
         solutions = self._get_solutions(demonstrations)
-        _ = self._get_predictions(demonstrations, solutions)
+        _ = self._trace_predictions(demonstrations, solutions)
         ranked_solutions = self._rank_solutions(demonstrations, solutions)
         return ranked_solutions[0]["page_content"], self.cost
