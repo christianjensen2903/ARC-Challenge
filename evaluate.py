23f67cb80118d9c17d837acc8c9fd7a561bdbc65
christianjensen2903 <cmj290300@gmail.com>
2024-10-16 11:54:13 +0200

    ✨ Running accuracy

--- evaluate.py
+++ evaluate.py
@@ -24,21 +24,33 @@ def evaluate(pipeline: Pipeline, n: int = 5) -> tuple[float, float]:
     challenges, solutions = load_data(train=False)
     ids = list(challenges.keys())
     correct = 0
-    cost = 0.0
-    for i in tqdm(range(n)):
+    total_cost = 0.0
+    progress_bar = tqdm(range(n), desc="Evaluating", unit="challenge")
+
+    for i in progress_bar:
         id = ids[i]
         prediction, cost = pipeline.solve(id)
         solution = np.array(solutions[id][0])
+
         if np.array_equal(prediction, solution):
             correct += 1
-        cost += cost
-    return correct / n, cost / n
+
+        total_cost += cost
+
+        # Update the progress bar with running accuracy
+        accuracy = correct / (i + 1)  # running accuracy
+        avg_cost = total_cost / (i + 1)  # running average cost
+        progress_bar.set_postfix(accuracy=accuracy, avg_cost=avg_cost)
+
+    return correct / n, total_cost / n
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.ERROR)
     model = ChatOpenAI(model="gpt-4o-mini")
-    formatter = EmojisDemonstrations()
+    formatter: DemonstrationFormatter = EmojisDemonstrations()
+    formatter = ShapeExtractionWrapper(formatter)
+    formatter = DifferenceWrapper(formatter)
     solver = COTSolver(model, formatter=formatter, num_examples=8, num_solutions=128)
     pipeline = Pipeline(demonstration_formatter=formatter, solver=solver)
     accuracy, avg_cost = evaluate(pipeline, n=50)
